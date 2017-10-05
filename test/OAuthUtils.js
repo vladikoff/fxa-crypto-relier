b143159c8bf1ef26fa8631b5df2d6c171c1dc95e
vladikoff <vlad.filippov@gmail.com>
2017-10-04 20:09:12 -0400

    Merge testing changes

--- test/OAuthUtils.js
+++ test/OAuthUtils.js
@@ -2,26 +2,51 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-describe('OAuthKeys', function () {
+describe('OAuthUtils', function () {
+  let keysJwk;
+  let encryptedBundle;
+
   const browserApi = {
     identity: {
-      launchWebAuthFlow: () => {
-        return Promise.resolve('url');
+      launchWebAuthFlow: (args) => {
+
+        keysJwk = args.url.split('&').pop().split('=')[1];
+
+        console.log(keysJwk);
+
+        const keysJwk2 = window.fxaCryptoDeriver.jose.util.base64url.decode(JSON.stringify(keysJwk));
+        const fxaDeriverUtils = new window.fxaCryptoDeriver.DeriverUtils();
+
+        return fxaDeriverUtils.encryptBundle(keysJwk2, JSON.stringify({cake: true}))
+          .then((bundle) => {
+            encryptedBundle = bundle;
+            console.log('encryptedBundle', encryptedBundle)
+            return 'wat';
+          });
       }
     }
+  };
+  const getBearerTokenRequest = function () {
+    return new Promise((resolve) => {
+      resolve({
+        derivedKeyBundle: encryptedBundle
+      });
+    });
   };
 
   const oAuthUtils = new window.fxaCryptoRelier.OAuthUtils();
 
-  it('should have HKDF work', (done) => {
-    oAuthUtils.launchFxaScopedKeyFlow({
+  it('should encrypt and decrypt the bundle', () => {
+    return oAuthUtils.launchFxaScopedKeyFlow({
       browserApi: browserApi,
-      pkce: true
+      getBearerTokenRequest: getBearerTokenRequest,
+      pkce: true,
     })
-    .then((key) => {
-      assert.equal(key.length, 64);
-      done();
-    });
+      .then((key) => {
+        console.log(key)
+        assert.equal(key.length, 64);
+      });
 
   });
+
 });
--- test/ScopedKeys.js
+++ test/ScopedKeys.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const assert = chai.assert;
+
 describe('ScopedKeys', function () {
   const scopedKeys = new window.fxaCryptoDeriver.ScopedKeys();
   const sampleKb = 'bc3851e9e610f631df94d7883d5defd5e5f55ab520bd5a9ae33dae26575c6b1a';
@@ -25,7 +27,6 @@ describe('ScopedKeys', function () {
         assert.equal(k.k.length, 43);
         assert.equal(k.kid, '20170510160522-O-ZDFFfhO2Tzpdag6csDmsRH2a1c9UMKlD0GdouP-5k');
         assert.equal(k.scope, scopedKeyIdentifier);
-
         window.crypto.subtle.importKey('jwk', k, importSpec, false, ['encrypt']).then(function (rawKey) {
           assert.equal(rawKey.type, 'secret');
           assert.equal(rawKey.usages[0], 'encrypt');
